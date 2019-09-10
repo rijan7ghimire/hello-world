34483ca3543fa1cf25b90c87ebcc783ca8765cf9
rijan7ghimire <55038884+rijan7ghimire@users.noreply.github.com>
2019-09-10 15:50:22 +0545

    Create addpositive.c

---
+++ addpositive.c
@@ -0,0 +1,23 @@
+
+
+#include<stdio.h>
+void main()
+ {
+     int n,m;
+
+        printf ("enter a numbers : \n");
+     scanf("%d",&n);
+
+   while(n<0)
+   {
+       printf("enter positive number only :");
+       scanf("%d",&n);}
+            {printf ("enter second numbers : \n");
+     scanf("%d",&m);}
+     while(m<0)
+   {
+       printf("enter positive number only :");
+       scanf("%d",&m);}
+printf("the sum is=%d",(m+n));
+   }
+
